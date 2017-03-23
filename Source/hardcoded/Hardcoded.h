621a7eb7546af63ee69809254f934096fbc26310
Joris de Jong <joris@hybridvisuals.nl>
2017-03-23 11:35:39 +0100

    Hardcoded class to temporarily create io and fixtures

---
+++ Source/hardcoded/Hardcoded.h
@@ -0,0 +1,18 @@
+/*
+  ==============================================================================
+
+    Hardcoded.h
+    Created: 23 Mar 2017 11:20:37am
+    Author:  Joris
+
+  ==============================================================================
+*/
+
+#ifndef HARDCODED_H_INCLUDED
+#define HARDCODED_H_INCLUDED
+
+
+
+
+
+#endif  // HARDCODED_H_INCLUDED
